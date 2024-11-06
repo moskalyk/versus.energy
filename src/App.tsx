b72d5168b79cb3e0d7cab1094fafada4f22a7671
moskalyk <morgan.moskalyk@protonmail.ch>
2024-11-06 17:28:45 -0500

    updated a cycle of images to not repeat images

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import defaultImageY from './assets/Y/default_y.png';
 import defaultImageX from './assets/X/default_x.png';
@@ -7,57 +7,72 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 
 function App() {
-  // Import all images in the assets folder
   const imagesY = import.meta.glob('./assets/Y/*.png', { eager: true });
   const imagesX = import.meta.glob('./assets/X/*.png', { eager: true });
-  // @ts-ignore
-  const imageArrayY = [defaultImageY, ...Object.values(imagesY).map((module) => module.default)]
-  // @ts-ignore
-  const imageArrayX = [defaultImageX, ...Object.values(imagesX).map((module) => module.default)]
+  const imageArrayY = [defaultImageY, ...Object.values(imagesY).map((module) => module.default)];
+  const imageArrayX = [defaultImageX, ...Object.values(imagesX).map((module) => module.default)];
 
+  const shuffleArray = (array: any) => array.sort(() => Math.random() - 0.5);
+
+  const [shuffledImagesY, setShuffledImagesY] = useState(shuffleArray([...imageArrayY]));
+  const [shuffledImagesX, setShuffledImagesX] = useState(shuffleArray([...imageArrayX]));
   const [imageIndexY, setImageIndexY] = useState(0);
   const [imageIndexX, setImageIndexX] = useState(0);
 
+  useEffect(() => {
+    if (imageIndexY >= shuffledImagesY.length) {
+      setShuffledImagesY(shuffleArray([...imageArrayY]));
+      setImageIndexY(0);
+    }
+  }, [imageIndexY, shuffledImagesY]);
+
+  useEffect(() => {
+    if (imageIndexX >= shuffledImagesX.length) {
+      setShuffledImagesX(shuffleArray([...imageArrayX]));
+      setImageIndexX(0);
+    }
+  }, [imageIndexX, shuffledImagesX]);
+
   const handleImageClickY = () => {
-    const randomIndex = Math.floor(Math.random() * imageArrayY.length);
-    setImageIndexY(randomIndex);
+    setImageIndexY((prevIndex) => prevIndex + 1);
   };
 
   const handleImageClickX = () => {
-    const randomIndex = Math.floor(Math.random() * imageArrayX.length);
-    setImageIndexX(randomIndex);
+    setImageIndexX((prevIndex) => prevIndex + 1);
   };
 
   return (
     <>
       <div>
-        <p style={{color: 'black'}}>Versus</p>
-        <br/>
-        <p style={{color: 'black'}}>Train against AI entropy</p>
-        <br/>
-        <br/>
+        <p style={{ color: 'black' }}>Versus</p>
+        <br />
+        <p style={{ color: 'black' }}>Train against AI entropy</p>
+        <br />
+        <br />
         <Swiper
           slidesPerView={1}
           spaceBetween={30}
-          loop={true}
+          loop={false}
           className="mySwiper"
         >
-          <SwiperSlide><img
-            src={imageArrayY[imageIndexY]}
-            className="logo"
-            alt="Cycling Image"
-            onClick={handleImageClickY}
-            style={{width: '350px', height: '350px', cursor: 'pointer'}}
-          /></SwiperSlide>
           <SwiperSlide>
-          <img
-            src={imageArrayX[imageIndexX]}
-            className="logo"
-            alt="Cycling Image"
-            onClick={handleImageClickX}
-            style={{width: '350px', height: '350px', cursor: 'pointer'}}
-          />
-            </SwiperSlide>
+            <img
+              src={shuffledImagesY[imageIndexY]}
+              className="logo"
+              alt="Cycling Image"
+              onClick={handleImageClickY}
+              style={{ width: '350px', height: '350px', cursor: 'pointer' }}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src={shuffledImagesX[imageIndexX]}
+              className="logo"
+              alt="Cycling Image"
+              onClick={handleImageClickX}
+              style={{ width: '350px', height: '350px', cursor: 'pointer' }}
+            />
+          </SwiperSlide>
         </Swiper>
       </div>
     </>
